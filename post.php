b58d27e6ee560510427a1a974b4275286697fa4e
Rae <58755365+raemx@users.noreply.github.com>
2023-04-18 17:11:57 +0100

    edit

--- post.php
+++ post.php
@@ -7,30 +7,53 @@ $message = $_POST["message"];
 
 require "vendor/autoload.php";
 
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 
-$mail = new PHPMailer(true);
+if(isset($_POST["send"])){
+  $mail = new PHPMailer(true);
+  $mail->isSMTP();
+  $mail->Host = "smtp.gmail.com";
+  $mail->SMTPAuth = true;
+  $mail->Username = 'onlinequran779@gmail.com';
+  $mail->password = 'zdspusvmzzjmkczf';
+  $mail->SMTPSecure = 'ssl';
+  $mail->Port = 465;
+  $mail->setFrom("onlinequran779@gmail.com");
+  $mail->addAddress($_POST["email"]);
+  $mail->isHTML(true);
+  $mail->Subject = "New Message from Arabic Tutors Website ";
+  $mail->Body = $message;
+  $mail->send();
 
-$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+}
 
-$mail->isSMTP();
-$mail->SMTPAuth = true;
+// $mail = new PHPMailer(true);
+// $mail->isSMTP();
+// $mail->SMTPDebug  = 1;  
+// $mail->SMTPAuth = true;
+// $mail->Host = "smtp.gmail.com";
+// $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+// $mail->Port = 587;
+// $mail->Username = "onlinequran779@gmail.com";
+// $mail->Password = "123ali123!";
+// // $mail->From = $email;
+// // $mail->FromName = $name;
+// $mail->setFrom($email, $name);
+// $mail->addAddress("onlinequran779@gmail.com", "Arabic Tutors");
+// $mail->Subject = "New Message from Arabic Tutors Website ";
+// $mail->Body = $message;
 
-$mail->Host = "smtp.gmail.com";
-$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-$mail->Port = 587;
+// $mail->send();
 
-$mail->Username = "onlinequran779@gmail.com";
-$mail->Password = "123ali123!";
+// $mail->MsgHTML($content); 
+// if(!$mail->Send()) {
+//   echo "Error while sending Email.";
+//   var_dump($mail);
+// } else {
+//     header("Location: contactsent.html");
+// }
 
-$mail->setFrom($email, $name);
-$mail->addAddress("raemel06@gmail.com", "Arabic Tutors");
 
-$mail->Subject = "New Message from Arabic Tutors Website ";
-$mail->Body = $message;
-
-$mail->send();
-
-header("Location: contactsent.html");
 ?>
